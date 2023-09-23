90c95d4a867af65f186310089658e1f21dc96562
Davis Weimer <davisweimer@gmail.com>
2023-09-23 15:17:42 -0600

    test: User Sad Path

--- spec/requests/api/v0/user_registration_spec.rb
+++ spec/requests/api/v0/user_registration_spec.rb
@@ -4,19 +4,15 @@ RSpec.describe "Users", type: :request do
   describe "POST /create" do
     it "creates a User from request", :vcr do
       user_request = {
-        user: {
-          email: "whatever@example.com",
-          password: "password",
-          password_confirmation: "password"
-        }
-      }
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password"
+      } 
       
       post api_v0_users_path, params: user_request.to_json, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
-
       expect(response).to have_http_status(:created)
 
       user = JSON.parse(response.body, symbolize_names: true)
-
       expect(user).to have_key(:data)
       expect(user[:data]).to have_key(:type)
       expect(user[:data]).to have_key(:id)
@@ -29,33 +25,28 @@ RSpec.describe "Users", type: :request do
       expect(user[:data][:attributes]).not_to have_key(:password)
     end
 
-    xit "returns status and error message on unsuccessful request", :vcr do
+    it "returns status and error message when email already exists and/or passwords don't match", :vcr do
       user_request = {
-        user: {
           email: "existing@example.com", 
           password: "yoooooooooo123",
           password_confirmation: "yoooooooooo123"
-        }
       }
 
       post api_v0_users_path, params: user_request.to_json, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
       invalid_user_request = {
-        user: {
           email: "existing@example.com", 
           password: "password",
-          password_confirmation: "password"
-        }
+          password_confirmation: "wuuuuuuuawhaowudh"
       }
 
       post api_v0_users_path, params: invalid_user_request.to_json, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
-
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.content_type).to include('application/json')
 
-      response_json = JSON.parse(response.body)
-
-      expect(response_json).to have_key('errors')
+      error = JSON.parse(response.body, symbolize_names: true)
+      expect(error).to have_key(:errors)
+      expect(error[:errors][0]).to eq("Email has already been taken")
+      expect(error[:errors][1]).to eq("Password confirmation doesn't match Password")
     end
   end
 end
