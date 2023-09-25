86a241ab311e11ad8d53694ece3ddcdae0939035
Davis Weimer <davisweimer@gmail.com>
2023-09-25 10:07:46 -0600

    refac: book-search test add .each for books

--- spec/requests/api/v0/book_search_spec.rb
+++ spec/requests/api/v0/book_search_spec.rb
@@ -14,41 +14,42 @@ RSpec.describe "BookSearches", type: :request do
   end
   describe "GET /show" do
     it "can search books based on a location name!", :vcr do
-      require 'pry'; binding.pry
       get api_v0_book_search_path, params: @book_query, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
       book_trip = JSON.parse(response.body, symbolize_names: true)
-
-      expect(road_trip).to have_key(:data)
-      expect(road_trip[:data]).to have_key(:type)
-      expect(road_trip[:data]).to have_key(:id)
-      expect(road_trip[:data][:id]).to eq(nil)
-      expect(road_trip[:data]).to have_key(:attributes)
-      expect(road_trip[:data][:type]).to eq("books")
-      expect(road_trip[:data][:attributes]).to have_key(:destination)
-      expect(road_trip[:data][:attributes][:destination]).to be_a(String)
+      expect(book_trip).to have_key(:data)
+      expect(book_trip[:data]).to have_key(:type)
+      expect(book_trip[:data]).to have_key(:id)
+      expect(book_trip[:data][:id]).to eq(nil)
+      expect(book_trip[:data]).to have_key(:attributes)
+      expect(book_trip[:data][:type]).to eq("books")
+      expect(book_trip[:data][:attributes]).to have_key(:destination)
+      expect(book_trip[:data][:attributes][:destination]).to be_a(String)
 
       # forecast
-      expect(road_trip[:data][:attributes]).to have_key(:forecast)
-      expect(road_trip[:data][:attributes][:forecast]).to be_a(Hash)
-      expect(road_trip[:data][:attributes][:forecast]).to have_key(:summary)
-      expect(road_trip[:data][:attributes][:forecast][:summary]).to be_a(String)
-      expect(road_trip[:data][:attributes][:forecast]).to have_key(:temperature)
-      expect(road_trip[:data][:attributes][:forecast][:temperature]).to be_a(String)
+      expect(book_trip[:data][:attributes]).to have_key(:forecast)
+      expect(book_trip[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(book_trip[:data][:attributes][:forecast]).to have_key(:summary)
+      expect(book_trip[:data][:attributes][:forecast][:summary]).to be_a(String)
+      expect(book_trip[:data][:attributes][:forecast]).to have_key(:temperature)
+      expect(book_trip[:data][:attributes][:forecast][:temperature]).to be_a(String)
 
       # total_books_found
-      expect(road_trip[:data][:attributes]).to have_key(:total_books_found)
-      expect(road_trip[:data][:attributes][:total_books_found]).to be_a(Integer)
+      expect(book_trip[:data][:attributes]).to have_key(:total_books_found)
+      expect(book_trip[:data][:attributes][:total_books_found]).to be_a(Integer)
       
       # books
-      expect(road_trip[:data][:attributes]).to have_key(:books)
-      expect(road_trip[:data][:attributes][:books]).to be_a(Hash)
-      expect(road_trip[:data][:attributes][:books].length).to eq(5)
-      expect(road_trip[:data][:attributes][:books]).to have_key(:isbn)
-      expect(road_trip[:data][:attributes][:books][:isbn]).to be_a(Array)
-      expect(road_trip[:data][:attributes][:books][:isbn].length).to eq(2)
-      expect(road_trip[:data][:attributes][:forecast]).to have_key(:title)
-      expect(road_trip[:data][:attributes][:forecast][:title]).to be_a(String)
+      expect(book_trip[:data][:attributes]).to have_key(:books)
+      expect(book_trip[:data][:attributes][:books]).to be_a(Array)
+      expect(book_trip[:data][:attributes][:books].length).to eq(5)
+
+      book_trip[:data][:attributes][:books].each do |book|
+        expect(book).to have_key(:isbn)
+        expect(book[:isbn]).to be_a(Array)
+        expect(book[:isbn].length).to eq(2)
+        expect(book).to have_key(:title)
+        expect(book[:title]).to be_a(String)
+      end
     end
   end
 end
