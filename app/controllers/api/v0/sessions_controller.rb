8eada4cdf38c59d6002fe4bcbd474fb2ddee8ba0
Davis Weimer <davisweimer@gmail.com>
2023-09-26 17:00:48 -0600

    refac: UserSerializer to JSONAPI::Serializer

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -7,7 +7,7 @@ module Api
         user = User.find_by(email: params[:email])
 
         if user&.authenticate(params[:password])
-          render json: UserSerializer.format_user(user), status: :ok
+          render json: UserSerializer.new(user), status: :ok
         else
           render json: { errors: 'Invalid email or password' }, status: :unauthorized
         end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
 class UserSerializer
-  def self.format_user(user)
-    {
-      data:
-      {
-        type: 'users',
-        id: user.id,
-        attributes: {
-          email: user.email,
-          api_key: user.api_key
-        }
-      }
-    }
-  end
+  include JSONAPI::Serializer
+  attributes :email, :api_key
 end
