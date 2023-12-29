4c76f2abf06af2d0aa5a97c33cf5c47486a886ee
Davis Weimer <davisweimer@gmail.com>
2023-12-29 14:21:29 -0700

    fix: Photo Endpoint updated

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -10,7 +10,7 @@ class PhotoService
   def self.find_photos_by_location_title(location)
     Rails.cache.fetch("backgrounds:#{location}", expires_in: 12.hours) do
       response = conn.get do |req|
-        req.url('search/photos', query: "daytime,cityscape,#{location}", orientation: 'landscape')
+        req.url('search/photos', query: location, orientation: 'landscape')
       end
       JSON.parse(response.body, symbolize_names: true)
     end
